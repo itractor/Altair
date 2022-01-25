7fbb92d012c3c493e69731fa0ea0324d61e096b7
oebars <oebars@gmail.com>
2022-01-25 21:57:41 +0100

    Added code for getting from server

---
+++ script.js
@@ -0,0 +1,32 @@
+function fetchTopStocks() {
+    fetch('http://localhost:8080/topStocks', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      })
+        .then(
+            function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' +
+                        response.status);
+                    return;
+                }
+
+                // Examine the text in the response
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+
+            }
+        ).catch(function(err) {
+            console.log('Fetch Error :-S', err);
+          });
+}
+
+window.onload = fetchTopStocks;
